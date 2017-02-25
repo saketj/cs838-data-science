c7e9e9911963b25976a1f6feb813f47efc8a92e1
Vishnu Sai Rao Lokhande <lokhande@wilfred.cs.wisc.edu>
2017-02-25 13:55:59 -0600

    positive samples extracted

--- stage-2/scripts/pos_extract.py
+++ stage-2/scripts/pos_extract.py
@@ -3,19 +3,19 @@ import re
 import glob
 
 pos_list = [['file name','start offset','end offset','dish name']] # A list for all positive examples
-path = '../datasets/text-documents-labeled/*.txt'
+path = '../datasets/text-documents-labeled_TRAIN/*.txt'
+# path = '../datasets/text-documents-labeled_TEST/*.txt'
 files = glob.glob(path)
 # iterate over files in the directory
 for fle in files:
    # opening the file and reading it as a string
    with open(fle) as f:
       text = f.read()
-      #str = '<test>some text here</test> <other> more text </other> <test> even more text</test>'
       match = re.compile('<dish>(.*?)</dish>', re.DOTALL).finditer(text)
       for l in match:
           pos_list.append([f.name,l.start(),l.end(),l.group(1)])
 
 # Writing output to csv
-with open("output.csv", "wb") as f:
+with open("../datasets/pos_extracted.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(pos_list)
