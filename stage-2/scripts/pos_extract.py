b246eaf639f99d02511d756911ce749f540b7c08
Vishnu Sai Rao Lokhande <lokhande@wilfred.cs.wisc.edu>
2017-02-25 13:51:01 -0600

    divided the labelled data into train and test sets

--- stage-2/scripts/pos_extract.py
+++ stage-2/scripts/pos_extract.py
@@ -1,17 +1,21 @@
 import csv
 import re
+import glob
 
-
-str = '<test>some text here</test> <other> more text </other> <test> even more text</test>'
-
-m2=re.compile('<test>(.*?)</test>', re.DOTALL).finditer(str)
-
-# creating a list of lists
-lists = []
-for l in lists:
-    lists.append([l.start(),l.end(),l.group(1)])
+pos_list = [['file name','start offset','end offset','dish name']] # A list for all positive examples
+path = '../datasets/text-documents-labeled/*.txt'
+files = glob.glob(path)
+# iterate over files in the directory
+for fle in files:
+   # opening the file and reading it as a string
+   with open(fle) as f:
+      text = f.read()
+      #str = '<test>some text here</test> <other> more text </other> <test> even more text</test>'
+      match = re.compile('<dish>(.*?)</dish>', re.DOTALL).finditer(text)
+      for l in match:
+          pos_list.append([f.name,l.start(),l.end(),l.group(1)])
 
 # Writing output to csv
 with open("output.csv", "wb") as f:
     writer = csv.writer(f)
-    writer.writerows(l)
+    writer.writerows(pos_list)
