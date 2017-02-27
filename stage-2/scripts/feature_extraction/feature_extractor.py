31c55bdd6d557ccf89b799d09cd4de0d24548ed3
Saket Saurabh <ssaurabh@cs.wisc.edu>
2017-02-27 00:52:41 -0600

    Add a basic feature extractor framework

---
+++ stage-2/scripts/feature_extraction/feature_extractor.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import arff
+import csv
+import sys
+
+import features
+
+
+def init_arff():
+    arff_content = {
+        'description': 'NYC Restaurant Review Dataset',
+        'relation': 'nyc_restaurant_review',
+        'attributes': [],
+        'data': []
+    }
+    return arff_content
+
+
+def init_features(arff_content):
+    list_of_attributes = define_attributes()
+    for attr in list_of_attributes:
+        arff_content['attributes'].append((attr.get_feature_name(), attr.get_feature_type()))
+    return arff_content
+
+
+def add_data(arff_content, input_filename):
+    list_of_attributes = define_attributes()
+    with open(input_filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            instance = []
+            for attr in list_of_attributes:
+                instance.append(attr.process(row))
+            arff_content['data'].append(instance)
+    return arff_content
+
+
+def define_attributes():
+    list_of_attributes = [
+                            features.FoodAdjectiveFeature(),
+                            features.FoodIngredientsFeature(),
+                            features.FoodAdjectiveContextFeature(),
+                            features.FoodIngredientsContextFeature(),
+                            features.HasPriceAttachedFeature(),
+                            features.DishNameFeature(),
+                            features.FileNameFeature()
+                         ]
+    return list_of_attributes
+
+
+def main(argv):
+    if len(argv) < 2:
+        sys.stderr.write("Incorrect arguments. Expecting two arguments: <input_file> <output_file>\n")
+        sys.exit(-1)
+    input_filename = argv[0]
+    output_file = open(argv[1], "wb")
+
+    arff_content = init_arff()
+    init_features(arff_content)
+    add_data(arff_content, input_filename)
+    arff.dump(arff_content, output_file)
+    output_file.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
