eb9b75e4fb56b261ed0715dd28a07eac40b4c6dd
Vishnu Sai Rao Lokhande <lokhande@wilfred.cs.wisc.edu>
2017-02-28 17:16:17 -0600

    Add a runner script

---
+++ stage-2/scripts/runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running for P.arff..."
+python scripts/feature_extraction/feature_extractor.py Debugging_stage/debug_P.arff Debugging_stage/debug_posextracted_P.csv Debugging_stage/debug_negextracted_P.csv
+
+echo "Running for Q.arff..."
+python scripts/feature_extraction/feature_extractor.py Debugging_stage/debug_Q.arff Debugging_stage/debug_posextracted_Q.csv Debugging_stage/debug_negextracted_Q.csv
+
+echo "Running for train.arff"
+python scripts/feature_extraction/feature_extractor.py datasets/features_TRAIN/features_train.arff datasets/pos_extracted.csv datasets/neg_extracted.csv
